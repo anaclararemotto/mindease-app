2ad4fe3f27395a549f16f7fa96fd841cb77effd2
Clara <clararemotto@gmail.com>
2026-02-09 11:25:08 -0300

    feat: configure splash

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{flex: 1}}>
+        
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
