407c61dfdcde9345b74911efa835d7388ce53d93
Clara <clararemotto@gmail.com>
2026-02-09 11:53:54 -0300

    feat: create landing base

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
-import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { LandingView } from "./screens/Landing/landing.view";
 
 export default function Index() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{flex: 1}}>
-        
+        <LandingView/>
       </SafeAreaView>
     </SafeAreaProvider>
   );
