2a98e94fd21a817511cf75d7a35f262b0db72ad1
Clara <clararemotto@gmail.com>
2026-02-09 12:43:24 -0300

    feat: create theme base

---
+++ src/shared/theme/themes.ts
@@ -0,0 +1,61 @@
+import * as C from './colors';
+import { ThemeName } from './types';
+
+export const themes: Record<ThemeName, C.Colors> = {
+  lightPurple: C.lightPurple,
+  darkPurple: C.darkPurple,
+
+  lightObsidian: C.lightObsidian,
+  darkObsidian: C.darkObsidian,
+
+  lightViolet: C.lightViolet,
+  darkViolet: C.darkViolet,
+
+  lightRed: C.lightRed,
+  darkRed: C.darkRed,
+
+  lightOrange: C.lightOrange,
+  darkOrange: C.darkOrange,
+
+  lightTanger: C.lightTanger,
+  darkTanger: C.darkTanger,
+
+  lightYellow: C.lightYellow,
+  darkYellow: C.darkYellow,
+
+  lightBrown: C.lightBrown,
+  darkBrown: C.darkBrown,
+
+  lightChocolate: C.lightChocolate,
+  darkChocolate: C.darkChocolate,
+
+  lightCinnamon: C.lightCinnamon,
+  darkCinnamon: C.darkCinnamon,
+
+  lightQuartz: C.lightQuartz,
+  darkQuartz: C.darkQuartz,
+
+  lightBlush: C.lightBlush,
+  darkBlush: C.darkBlush,
+
+  lightPeony: C.lightPeony,
+  darkPeony: C.darkPeony,
+
+  lightBlueSky: C.lightBlueSky,
+  darkBlueSky: C.darkBlueSky,
+
+  lightCobalt: C.lightCobalt,
+  darkCobalt: C.darkCobalt,
+
+  lightPolar: C.lightPolar,
+  darkPolar: C.darkPolar,
+
+  lightCian: C.lightCian,
+  darkCian: C.darkCian,
+
+  lightGreen: C.lightGreen,
+  darkGreen: C.darkGreen,
+
+  lightForest: C.lightForest,
+  darkForest: C.darkForest,
+};
