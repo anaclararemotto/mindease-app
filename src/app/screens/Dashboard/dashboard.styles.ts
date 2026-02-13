2721eb3126d041c7bd30582fb64e80091bb63a7d
Clara <clararemotto@gmail.com>
2026-02-13 11:43:28 -0300

    feat: add content to dashboard page

--- src/app/screens/Dashboard/dashboard.styles.ts
+++ src/app/screens/Dashboard/dashboard.styles.ts
@@ -1,10 +1,34 @@
 import { Colors } from '@/src/shared/theme/colors';
+import { typography } from '@/src/shared/theme/typography';
 import { StyleSheet } from 'react-native';
 
 export const dashboardStyles = (colors: Colors) =>
   StyleSheet.create({
     container: {
+      backgroundColor: colors.background,
       paddingHorizontal: 30,
-      paddingVertical: 90
+      paddingVertical: 90,
+      height: '100%',
+      gap: 20,
+      alignItems: 'center'
+    },
+    containerText: {
+      gap: 5,
+      width: '100%'
+    },
+    title: {
+      ...typography.headingmd,
+      color: colors.colorPrimary
+    },
+    subtitle: {
+      ...typography.bodylg,
+      color: colors.textColor
+    },
+    image: {
+      width: "90%",
+    },
+    containerOptions: {
+      width: "100%",
+      gap: 20,
     },
   });
--- src/app/screens/Dashboard/dashboard.view.tsx
+++ src/app/screens/Dashboard/dashboard.view.tsx
@@ -1,8 +1,10 @@
 import { useTheme } from "@/src/shared/theme/ThemeContext";
-import { CalendarDays } from "lucide-react";
-import { View } from "react-native";
+import { CalendarDays, Goal } from "lucide-react";
+import { Image, Text, View } from "react-native";
 import { OptionCard } from "../../components/OptionCard/optioncard";
 import { dashboardStyles } from "./dashboard.styles";
+import DashboardLight from "@/assets/images/dashboard-light.svg";
+
 
 export const DashboardView = () => {
   const { colors } = useTheme();
@@ -10,7 +12,17 @@ export const DashboardView = () => {
 
   return (
     <View style={styles.container}>
-        <OptionCard label="Cronograma do dia" icon={CalendarDays}/>
+      <View  style={styles.containerText}>
+        <Text  style={styles.title}>Olá Joana!</Text>
+        <Text  style={styles.subtitle}>Comece devagar e siga no seu tempo.</Text>
+      </View>
+      <Image source={DashboardLight} style={styles.image}
+          resizeMode="contain"/>
+
+      <View style={styles.containerOptions}>
+        <OptionCard label="Cronograma do dia" icon={CalendarDays} />
+        <OptionCard label="Desafio de hoje" icon={Goal} />
+      </View>
     </View>
   );
 };
